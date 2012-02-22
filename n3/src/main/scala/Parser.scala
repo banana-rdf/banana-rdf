c4e72bd8635c1bcfb544bcca269228c52b3cd673
Henry Story <henry.story@bblfish.net>
2012-02-22 01:13:47 +0100

    lazy vals are test best solution to avoid https://bitbucket.org/pchiusano/nomo/issue/5/null-pointer-exception

--- n3/src/main/scala/Parser.scala
+++ n3/src/main/scala/Parser.scala
@@ -181,37 +181,41 @@ class TurtleParser[M <: RDFModule,F,E,X,U <: ListenerAgent[Any]](val m: M, val P
   import TurtleParser._
   import P.++
 
+  /**
+   * Note without lazy val, the order of the parsers would be important
+   */
+
   /** Parses the single token given that matches the function */
   def single(isC: Char => Boolean ): P.Parser[Char] = P.any mapResult (s =>
     s.status.flatMap(i => if (isC(i) ) Success(i) else Failure(P.err.single(i, s.position))))
 
-  val err = (pos: X) =>P.err.single('!',pos)
-  val COLON = P.single(':')
-  val PREFIX = P.word("@prefix")
-  val dot = P.single('.')
-  val eoln = P.word ("\r\n") | P.word("\n")  | P.word("\r")
+  lazy val err = (pos: X) =>P.err.single('!',pos)
+  lazy val COLON = P.single(':')
+  lazy val PREFIX = P.word("@prefix")
+  lazy val dot = P.single('.')
+  lazy val eoln = P.word ("\r\n") | P.word("\n")  | P.word("\r")
 
-  val comment = P.single('#')>>P.takeWhile(c=> c != '\r' && c != '\n') << eoln
-  val SP = (P.takeWhile1(c=> " \t\r\n".contains(c),err) | comment ).many1
+  lazy val SP = (P.takeWhile1(c=> " \t\r\n".contains(c),err) | comment ).many1
+  lazy val comment = P.single('#')>>P.takeWhile(c=> c != '\r' && c != '\n') << eoln
 
-  val hexadecimalChars = "1234567890ABCDEFabcdef"
-  def hex = P.anyOf(hexadecimalChars)
+  lazy val hexadecimalChars = "1234567890ABCDEFabcdef"
+  lazy val hex = P.anyOf(hexadecimalChars)
 
-  val u_CHAR = (P.word("\\u")>> hex++hex++hex++hex) map {
+  lazy val u_CHAR = (P.word("\\u")>> hex++hex++hex++hex) map {
     case c1++c2++c3++c4 => Integer.parseInt(new String(Array(c1,c2,c3,c4)),16).toChar
   }
-  val U_CHAR = (P.word("\\U")>> hex++hex++hex++hex++hex++hex++hex++hex) map {
+  lazy val U_CHAR = (P.word("\\U")>> hex++hex++hex++hex++hex++hex++hex++hex) map {
     case c1++c2++c3++c4++c5++c6++c7++c8 => Integer.parseInt(new String(Array(c1,c2,c3,c4,c5,c6,c7,c8)),16).toChar
   }
 
-  val UCHAR = u_CHAR | U_CHAR
-  val UCHARS = UCHAR.many1.map(_.mkString)
-  val PN_CHARS_BASE = single(pn_chars_simple) | UCHAR
-  val PN_CHARS_U = PN_CHARS_BASE | P.single ('_')
-  val PN_CHARS =  P.takeWhile1(pn_chars_dot, err).map(_.toSeq.mkString) | UCHAR.many1.map(_.toSeq.mkString)
-  val PN_decode = PN_CHARS.many1.map(_.mkString)
+  lazy val UCHAR = u_CHAR | U_CHAR
+  lazy val UCHARS = UCHAR.many1.map(_.mkString)
+  lazy val PN_CHARS_BASE = single(pn_chars_simple) | UCHAR
+  lazy val PN_CHARS_U = PN_CHARS_BASE | P.single ('_')
+  lazy val PN_CHARS =  P.takeWhile1(pn_chars_dot, err).map(_.toSeq.mkString) | UCHAR.many1.map(_.toSeq.mkString)
+  lazy val PN_decode = PN_CHARS.many1.map(_.mkString)
 
-  val PN_PREFIX = P.takeWhile(_ != ':').mapResult {
+  lazy val PN_PREFIX = P.takeWhile(_ != ':').mapResult {
     case Result(Success(pfx), pos, us)=> {
       val prefx = pfx.toSeq.mkString
       if (prefx.size == 0) Success("")
@@ -225,14 +229,14 @@ class TurtleParser[M <: RDFModule,F,E,X,U <: ListenerAgent[Any]](val m: M, val P
     case other => other.status
   }
 
-  val PNAME_NS =  PN_PREFIX << COLON
-  val IRI_REF =  P.single('<')>>(P.takeWhile1(iri_char, pos =>P.err.single('!',pos)) | UCHARS).many.map(_.mkString)<<P.single('>')
-  val PREFIX_Part1 = PREFIX >> SP >> PNAME_NS
-  val prefixID =  PREFIX_Part1 ++ SP>>IRI_REF
-  val directive = prefixID //| base
+  lazy val PNAME_NS =  PN_PREFIX << COLON
+  lazy val IRI_REF =  P.single('<')>>(P.takeWhile1(iri_char, pos =>P.err.single('!',pos)) | UCHARS).many.map(_.mkString)<<P.single('>')
+  lazy val PREFIX_Part1 = PREFIX >> SP >> PNAME_NS
+  lazy val prefixID =  PREFIX_Part1 ++ SP>>IRI_REF
+  lazy val directive = prefixID //| base
 
-  val statement = ( directive << dot ) //| ( turtleTriples << dot )
-  val turtleDoc = statement.many
+  lazy val statement = ( directive << dot ) //| ( turtleTriples << dot )
+  lazy val turtleDoc = statement.many
 
 }
 
