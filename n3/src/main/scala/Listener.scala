b71e54a98ac1130170c9d18c0ba366c0786012c4
Henry Story <henry.story@bblfish.net>
2012-02-25 10:09:37 +0100

    minor documentation improvements.

--- n3/src/main/scala/Listener.scala
+++ n3/src/main/scala/Listener.scala
@@ -3,6 +3,11 @@ package org.w3.rdf.n3
 import collection.{immutable, mutable}
 import org.w3.rdf._
 
+/**
+ * An agent that collects triples as they are built up and places them in a
+ * structure which an other process can consume.
+ * for a discussion see: https://bitbucket.org/pchiusano/nomo/issue/3/partial-results
+ */
 case class Listener[RDF <: RDFDataType](val ops: RDFOperations[RDF]) {
   
   import ops._
@@ -10,6 +15,7 @@ case class Listener[RDF <: RDFDataType](val ops: RDFOperations[RDF]) {
   private val prefixs = new mutable.HashMap[String, RDF#IRI]
   val queue: mutable.Queue[RDF#Triple] = new mutable.Queue[RDF#Triple]()
 
+  // Perhaps it could send the triple to an agent here
   def send(a: RDF#Triple) = queue.enqueue(a)
 
   def addPrefix(name: String, value: RDF#IRI) {
