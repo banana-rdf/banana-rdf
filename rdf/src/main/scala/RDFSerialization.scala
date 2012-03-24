e7b144e21a60031f1b2be6b9d07ddaa8754b6221
Henry Story <henry.story@bblfish.net>
2012-03-24 21:18:26 +0100

    added rdfa html and xhtml parser constants

--- rdf/src/main/scala/RDFSerialization.scala
+++ rdf/src/main/scala/RDFSerialization.scala
@@ -13,3 +13,12 @@ case object Turtle extends Turtle
 
 trait N3 extends RDFSerialization
 case object N3 extends N3
+
+trait RDFa extends RDFSerialization
+case object RDFa extends RDFa
+
+trait RDFaXHTML extends RDFSerialization
+case object RDFaXHTML extends RDFa
+
+trait RDFaHTML extends RDFSerialization
+case object RDFaHTML extends RDFa
