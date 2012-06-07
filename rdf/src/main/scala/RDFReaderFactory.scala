8024443ec1c80069c2cf86e07d8e6323db59ed0b
Henry Story <henry.story@bblfish.net>
2012-06-07 23:06:37 +0200

    RdfRdrFactory.find( Serialisation):Option[Reader]

--- rdf/src/main/scala/RDFReaderFactory.scala
+++ rdf/src/main/scala/RDFReaderFactory.scala
@@ -6,6 +6,13 @@ import scalaz.Validation
 import scalaz.Validation._
 
 trait RDFReaderFactory[Rdf <: RDF] {
+
+  def find(ser: RDFSerialization): Option[RDFReader[Rdf,_]] = ser match {
+    case RDFXML => Some(RDFXMLReader)
+    case Turtle => Some(TurtleReader)
+    case _ => None
+  }
+
   def RDFXMLReader: RDFReader[Rdf, RDFXML]
   def TurtleReader: RDFReader[Rdf, Turtle]
 }
