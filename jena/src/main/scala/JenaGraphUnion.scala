0223a483a9eae9ba1505c607850f6e53b52e4d18
Alexandre Bertails <bertails@gmail.com>
2012-06-17 22:54:29 -0400

    added some tests for GraphUnion

--- jena/src/main/scala/JenaGraphUnion.scala
+++ jena/src/main/scala/JenaGraphUnion.scala
@@ -7,10 +7,16 @@ import JenaOperations.Graph.toIterable
 object JenaGraphUnion extends GraphUnion[Jena] {
   
   def union(left: Jena#Graph, right: Jena#Graph): Jena#Graph = {
-    val graph = Factory.createDefaultGraph
-    toIterable(left) foreach { t => graph add t }
-    toIterable(right) foreach { t => graph add t }
-    graph
+    if (left.isEmpty)
+      right
+    else if (right.isEmpty)
+      left
+    else {
+      val graph = Factory.createDefaultGraph
+      toIterable(left) foreach { t => graph add t }
+      toIterable(right) foreach { t => graph add t }
+      graph
+    }
   }
   
 }
