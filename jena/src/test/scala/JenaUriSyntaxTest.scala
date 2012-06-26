9216267aefc1f5d57550769a45c9468a27ee71a7
Alexandre Bertails <bertails@w3.org>
2012-06-26 10:01:04 -0400

    s/supportDocument/fragmentLess/ in UriSyntax, and added a method to remove the fragment part of a URI

---
+++ jena/src/test/scala/JenaUriSyntaxTest.scala
@@ -0,0 +1,6 @@
+package org.w3.banana.jena
+
+import org.w3.banana._
+import Jena._
+
+class JenaUriSyntaxTest extends UriSyntaxTest[Jena]
---
+++ rdf-test-suite/src/main/scala/UriSyntaxTest.scala
@@ -0,0 +1,21 @@
+package org.w3.banana
+
+import org.scalatest._
+import org.scalatest.matchers.MustMatchers
+
+abstract class UriSyntaxTest[Rdf <: RDF]()(implicit diesel: Diesel[Rdf]) extends WordSpec with MustMatchers {
+
+  import diesel._
+
+  "URI.fragmentLess should remove the fragment part of a URI" in {
+    val uri = URI("http://example.com/foo#bar")
+    uri.fragmentLess must be (URI("http://example.com/foo"))
+  }
+
+  "URI.fragment should set the fragment part of a URI" in {
+    val uri = URI("http://example.com/foo")
+    uri.fragment("bar") must be (URI("http://example.com/foo#bar"))
+  }
+
+
+}
