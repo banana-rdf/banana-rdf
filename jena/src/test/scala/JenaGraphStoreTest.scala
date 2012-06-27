a81f3da0ccd3c80b969dde620e058f69ab22cd63
Jonathan WINANDY <jonathan.winandy@gmail.com>
2012-06-27 17:08:19 +0200

    fixed JenaTDB bug

--- jena/src/test/scala/JenaGraphStoreTest.scala
+++ jena/src/test/scala/JenaGraphStoreTest.scala
@@ -4,16 +4,20 @@ import org.w3.banana._
 import com.hp.hpl.jena.sparql.core._
 import Jena._
 import JenaRDFReader._
-import com.hp.hpl.jena.tdb.TDBFactory
+import com.hp.hpl.jena.tdb.{TDB, TDBFactory}
 
 
-abstract class AbstractJenaGraphStoreTest(val jenaStore: RDFStore[Jena, JenaSPARQL]) extends GraphStoreTest[Jena](jenaStore) {
+abstract class AbstractJenaGraphStoreTest(val jenaStore: JenaStore) extends GraphStoreTest[Jena](jenaStore) {
 
   import JenaOperations._
 
+  def defaultGraph: Jena#Graph = jenaStore.readTransaction {
+    jenaStore.dg.getDefaultGraph
+  }
+
   "adding a named graph should not pollute the default graph" in {
     store.addNamedGraph(makeUri("http://example.com/foo"), graph)
-    val defaultGraph = store.getNamedGraph(null.asInstanceOf[Jena#URI])
+
     defaultGraph must have size(0)
   }
 
@@ -21,5 +25,6 @@ abstract class AbstractJenaGraphStoreTest(val jenaStore: RDFStore[Jena, JenaSPAR
 
 class MemJenaGraphStore extends AbstractJenaGraphStoreTest(JenaStore(DatasetGraphFactory.createMem()))
 
-class TDBJenaGraphStore extends AbstractJenaGraphStoreTest(JenaStore(TDBFactory.createDataset("test.tdb")))
-
+class TDBJenaGraphStore extends AbstractJenaGraphStoreTest(JenaStore(TDBFactory.createDataset("test.tdb"))) {
+  TDB.getContext().set(TDB.symUnionDefaultGraph, false)
+}
