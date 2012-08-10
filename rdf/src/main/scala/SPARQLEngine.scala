33d23a1c74f17c1c152620dccba7679b3589bd45
Henry Story <henry.story@bblfish.net>
2012-08-10 15:31:34 +0200

    remove commented code in MSPARQLEngine. Use OpenSPARQLEngine.

--- rdf/src/main/scala/SPARQLEngine.scala
+++ rdf/src/main/scala/SPARQLEngine.scala
@@ -1,11 +1,7 @@
 package org.w3.banana
 
 import util._
-import scalaz._
 import scalaz.Id
-import scalaz.Right3
-import scalaz.Middle3
-import scalaz.Left3
 
 trait SPARQLEngine[Rdf <: RDF] extends MSPARQLEngine[Rdf, Id]
 
@@ -27,24 +23,5 @@ trait MSPARQLEngine[Rdf <: RDF, M[_]] {
   def executeConstruct(query: Rdf#ConstructQuery): M[Rdf#Graph] = executeConstruct(query, Map.empty)
 
   def executeAsk(query: Rdf#AskQuery): M[Boolean] = executeAsk(query, Map.empty)
-
-//
-// type Answer = Either3[Rdf#Solutions, Rdf#Graph, Boolean]
-//
-// It would be good to be able to add the following here:
-//
-//  /**
-//   * This takes a generic query, and returns whatever type of object that query returns
-//   * @param query
-//   * @return an Iterable[Rdf#Row] if the query was a select query,
-//   *         an Rdf#Graph if the query was a Construct query
-//   *         a boolean if the query was an ASK query
-//   */
-//  def executeQuery(query: Rdf#Query): M[ = ops.fold(query)(
-//    select => Left3(executeSelect(graph, select)),
-//    construct => Middle3(executeConstruct(graph, construct)),
-//    ask => Right3(executeAsk(graph, ask))
-//  )
-
 
 }
