8838bdc9d76421c3901edbfed22e5605d94348b9
Alexandre Bertails <bertails@gmail.com>
2012-09-06 11:02:40 -0400

    both implementation and tests for banana-jena are ok

--- jena/src/test/scala/JenaGraphStoreTest.scala
+++ jena/src/test/scala/JenaGraphStoreTest.scala
@@ -3,27 +3,27 @@ package org.w3.banana.jena
 import org.w3.banana._
 import com.hp.hpl.jena.sparql.core._
 import Jena._
-import JenaRDFReader._
 import com.hp.hpl.jena.tdb.{ TDB, TDBFactory }
 import JenaOperations._
 
-
-class JenaGraphStoreTest(val jenaStore: JenaStore) extends GraphStoreTest[Jena](jenaStore) {
-
+abstract class JenaGraphStoreTest(jenaStore: JenaStore) extends GraphStoreTest[Jena](jenaStore) {
 
   def defaultGraph: Jena#Graph = jenaStore.readTransaction {
     BareJenaGraph(jenaStore.dg.getDefaultGraph)
   }
 
   "adding a named graph should not pollute the default graph" in {
-    store.appendToGraph(makeUri("http://example.com/foo"), graph)
+    val s = jenaStore.execute {
+      Command.append[Jena](makeUri("http://example.com/foo"), graphToIterable(graph))
+    }
+    s.getOrFail()
     defaultGraph.jenaGraph must have size(0)
   }
 
 }
 
-class JenaMemGraphStore extends AbstractJenaGraphStoreTest(JenaStore(DatasetGraphFactory.createMem()))
+class JenaMemGraphStoreTest extends JenaGraphStoreTest(JenaStore(DatasetGraphFactory.createMem()))
 
-class JenaTDBGraphStore extends AbstractJenaGraphStoreTest(JenaStore(TDBFactory.createDataset("test.tdb"), defensiveCopy = true)) {
+class JenaTDBGraphStoreTest extends JenaGraphStoreTest(JenaStore(TDBFactory.createDataset("test.tdb"), defensiveCopy = true)) {
   TDB.getContext().set(TDB.symUnionDefaultGraph, false)
 }
