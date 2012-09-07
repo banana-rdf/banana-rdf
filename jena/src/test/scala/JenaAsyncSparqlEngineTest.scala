373ea1da1adfd26e8b0d37a3194c3d28108b6122
Alexandre Bertails <bertails@gmail.com>
2012-09-07 09:59:05 -0400

    forgot to git rm some file

--- jena/src/test/scala/JenaAsyncSparqlEngineTest.scala
+++
@@ -1,9 +0,0 @@
-package org.w3.banana.jena
-
-import org.w3.banana._
-import com.hp.hpl.jena.sparql.core._
-import Jena._
-import JenaRDFReader._
-
-class JenaAsyncSparqlEngineTest() extends AsyncSparqlEngineTest[Jena](JenaStore(DatasetGraphFactory.createMem()))
-
