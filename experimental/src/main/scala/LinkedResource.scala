1fdb1d8f3a6573abf6c6c592d92d5ae9cb008829
Alexandre Bertails <bertails@gmail.com>
2013-01-06 21:52:38 -0500

    defined LinkedResource

---
+++ experimental/src/main/scala/LinkedResource.scala
@@ -0,0 +1,43 @@
+package org.w3.banana.experimental
+
+import org.w3.banana._
+import scala.concurrent._
+
+/** A resource that can be find with its URI, and is linked to other
+  * resources through links as URIs
+  * 
+  * @tparam Rdf an RDF implementation
+  * @tparam the kind of resources that can be linked
+  */
+trait LinkedResource[Rdf <: RDF, LR] {
+
+  /** retrieves a resource based on its URI */
+  def ~(uri: Rdf#URI): Future[LR]
+
+  /** follow the  */
+  def ~> (lr: LR, predicate: Rdf#URI): Iterable[Future[LR]]
+
+}
+
+/** A [[LinkedDataResource]] is obviously a [[LinkedResource]] */
+class LDRLinkedResource[Rdf <: RDF]()(implicit ops: RDFOps[Rdf]) extends LinkedResource[Rdf, LinkedDataResource[Rdf]] {
+
+  def ~(uri: Rdf#URI): Future[LinkedDataResource[Rdf]] = ???
+
+  def ~> (lr: LinkedDataResource[Rdf], predicate: Rdf#URI): Iterable[Future[LinkedDataResource[Rdf]]] = ???
+  
+
+}
+
+/** Resources within a same RDF graph are linked together */
+class PointedGraphLinkedResource[Rdf <: RDF](pg: PointedGraph[Rdf])(implicit ops: RDFOps[Rdf]) extends LinkedResource[Rdf, PointedGraph[Rdf]] {
+
+  def ~(uri: Rdf#URI): Future[PointedGraph[Rdf]] =
+    Future.successful(PointedGraph(uri, pg.graph))
+
+  def ~> (lr: PointedGraph[Rdf], predicate: Rdf#URI): Iterable[Future[PointedGraph[Rdf]]] = {
+    val nodes = ops.getObjects(pg.graph, pg.pointer, predicate)
+    nodes map { node => Future.successful(PointedGraph(node, pg.graph)) }
+  }
+  
+}
