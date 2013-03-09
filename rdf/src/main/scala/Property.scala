627b9955374d43da16265e64fe15cb29db33bc0f
Artem Kozlov <artem.akozlov@gmail.com>
2013-03-10 00:22:13 +0200

    Example of record binder with custom id field.

--- rdf/src/main/scala/Property.scala
+++ rdf/src/main/scala/Property.scala
@@ -4,16 +4,8 @@ import scala.util._
 
 trait Property[Rdf <: RDF, T] {
 
-  def uri: Rdf#URI
-
   def pos(t: T): Iterable[(Rdf#URI, PointedGraph[Rdf])]
 
   def extract(pointed: PointedGraph[Rdf]): Try[T]
-
-}
-
-object Property {
-
-  implicit def propertyToUri[Rdf <: RDF](p: Property[Rdf, _]): Rdf#URI = p.uri
 
 }
