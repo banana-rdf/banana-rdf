130bf06b69934a4149bfb5fe56512384a0c417d2
Henry Story <henry.story@bblfish.net>
2013-11-15 11:21:52 +0100

    PointedGraph tests to help with documentation

---
+++ rdf-test-suite/src/main/scala/PointedGraphTester.scala
@@ -0,0 +1,40 @@
+package org.w3.banana
+
+import org.scalatest.{Matchers, WordSpec}
+
+/**
+ *  see org.w3.banana.binder.CommonBindersTest for more tests
+ */
+class PointedGraphTester[Rdf<:RDF]()(implicit ops: RDFOps[Rdf])
+  extends WordSpec with Matchers {
+
+  import ops._
+
+  val henryURI: String = "http://bblfish.net/people/henry/card#me"
+  val henry = URI(henryURI)
+
+
+  "Two similarly constructed PointedGraphs only have plain object identity"  when {
+
+    "with URI pointers" in {
+      val pg1 = PointedGraph(henry,Graph.empty)
+      val pg2 = PointedGraph(URI(henryURI))
+      pg1.pointer should be (pg2.pointer)
+      pg1.graph should be (pg2.graph)
+      //yet
+      pg1 should not be pg2
+    }
+
+    "with bnode pointers" in {
+        val bnode = BNode()
+        val graph = Graph(Triple(bnode,rdf.first,LangLiteral("Henry",Lang("en"))))
+        val pg1 = PointedGraph(bnode,graph)
+        val pg2 = PointedGraph(bnode,graph)
+
+        pg1 should not be pg2
+    }
+
+
+  }
+
+}
