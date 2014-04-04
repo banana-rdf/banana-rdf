95069024cf0184172c6dba9fc0be55efbeb5b863
Alexandre Bertails <bertails@gmail.com>
2014-04-04 00:12:27 -0400

    Adds support for Union Graph as Default Graph in Jena -- closes #95

--- jena/src/main/scala/jena/JenaGraphSparqlEngine.scala
+++ jena/src/main/scala/jena/JenaGraphSparqlEngine.scala
@@ -7,33 +7,60 @@ import com.hp.hpl.jena.query._
 import scala.concurrent.Future
 import scala.concurrent.Future.successful
 
+class JenaSparqlEngine(ops: RDFOps[Jena], graph: Jena#Graph) extends SparqlEngine[Jena] {
+
+  val querySolution = new util.QuerySolution(ops)
+
+  def qexec(query: Jena#Query, bindings: Map[String, Jena#Node]): QueryExecution = {
+    val model: Model = ModelFactory.createModelForGraph(graph)
+    if (bindings.isEmpty)
+      QueryExecutionFactory.create(query, model)
+    else
+      QueryExecutionFactory.create(query, model, querySolution.getMap(bindings))
+  }
+
+  def executeSelect(query: Jena#SelectQuery, bindings: Map[String, Jena#Node]): Future[Jena#Solutions] = successful {
+    qexec(query, bindings).execSelect()
+  }
+
+  def executeConstruct(query: Jena#ConstructQuery, bindings: Map[String, Jena#Node]): Future[Jena#Graph] = successful {
+    val result = qexec(query, bindings).execConstruct()
+    result.getGraph()
+  }
+
+  def executeAsk(query: Jena#AskQuery, bindings: Map[String, Jena#Node]): Future[Boolean] = successful {
+    qexec(query, bindings).execAsk()
+  }
+
+}
+
+
 class JenaSparqlGraph(ops: RDFOps[Jena]) extends SparqlGraph[Jena] {
 
-  def apply(graph: Jena#Graph): SparqlEngine[Jena] = new SparqlEngine[Jena] {
+  def apply(graph: Jena#Graph): SparqlEngine[Jena] =
+    new JenaSparqlEngine(ops, graph)
 
-    val querySolution = new util.QuerySolution(ops)
+}
 
-    def qexec(query: Jena#Query, bindings: Map[String, Jena#Node]): QueryExecution = {
-      val model: Model = ModelFactory.createModelForGraph(graph)
-      if (bindings.isEmpty)
-        QueryExecutionFactory.create(query, model)
-      else
-        QueryExecutionFactory.create(query, model, querySolution.getMap(bindings))
-    }
 
-    def executeSelect(query: Jena#SelectQuery, bindings: Map[String, Jena#Node]): Future[Jena#Solutions] = successful {
-      qexec(query, bindings).execSelect()
-    }
+/** see
+  * - https://jena.apache.org/documentation/tdb/datasets.html
+  * - http://jena.apache.org/documentation/tdb/assembler.html#union-default-graph
+  */
+trait UnionGraph extends JenaSparqlEngine {
 
-    def executeConstruct(query: Jena#ConstructQuery, bindings: Map[String, Jena#Node]): Future[Jena#Graph] = successful {
-      val result = qexec(query, bindings).execConstruct()
-      result.getGraph()
-    }
-
-    def executeAsk(query: Jena#AskQuery, bindings: Map[String, Jena#Node]): Future[Boolean] = successful {
-      qexec(query, bindings).execAsk()
-    }
-
+  override def qexec(query: Jena#Query, bindings: Map[String, Jena#Node]): QueryExecution = {
+    val qe = super.qexec(query, bindings)
+    import com.hp.hpl.jena.tdb.TDB
+    qe.getContext().set(TDB.symUnionDefaultGraph, true)
+    qe
   }
+
+}
+
+class JenaSparqlUnionGraph(ops: RDFOps[Jena]) extends SparqlGraph[Jena] {
+
+  def apply(graph: Jena#Graph): SparqlEngine[Jena] =
+    new JenaSparqlEngine(ops, graph) with UnionGraph
 
 }
