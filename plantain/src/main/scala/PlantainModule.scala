fcae2932d75194b825592b04fa94c2d2689c6f14
Alexandre Bertails <bertails@gmail.com>
2014-05-23 14:30:53 -0400

    wired all tests that could be wired in Plantain

--- plantain/src/main/scala/PlantainModule.scala
+++ plantain/src/main/scala/PlantainModule.scala
@@ -5,12 +5,15 @@ import org.w3.banana._
 trait PlantainModule
 extends RDFModule
 with RDFOpsModule
+with RecordBinderModule
 with TurtleReaderModule
 with TurtleWriterModule {
 
   type Rdf = Plantain
 
   implicit val Ops: RDFOps[Plantain] = PlantainOps
+
+  implicit val RecordBinder: binder.RecordBinder[Plantain] = binder.RecordBinder[Plantain]
 
   implicit val TurtleReader: RDFReader[Plantain, Turtle] = PlantainTurtleReader
 
