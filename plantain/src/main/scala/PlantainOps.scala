cf1d3446e1cf920e03d22c164238c13ada5c3161
Alexandre Bertails <bertails@gmail.com>
2014-05-24 18:29:01 -0400

    moved PlantainURIOps in its own file

--- plantain/src/main/scala/PlantainOps.scala
+++ plantain/src/main/scala/PlantainOps.scala
@@ -4,48 +4,6 @@ import org.w3.banana._
 import model._
 import spray.http.Uri
 
-trait PlantainURIOps extends URIOps[Plantain] {
-
-  def getString(uri: Plantain#URI): String = uri.underlying.toString
-
-  def withoutFragment(uri: Plantain#URI): Plantain#URI =
-    URI(uri.underlying.withoutFragment)
-
-  def withFragment(uri: Plantain#URI, frag: String): Plantain#URI =
-    URI(uri.underlying.withFragment(frag))
-
-  def getFragment(uri: Plantain#URI): Option[String] = uri.underlying.fragment
-
-  def isPureFragment(uri: Plantain#URI): Boolean = uri.underlying.fragment.isDefined
-
-  def resolve(uri: Plantain#URI, other: Plantain#URI): Plantain#URI = {
-    URI(other.underlying.resolvedAgainst(uri.underlying))
-  }
-
-  def appendSegment(uri: Plantain#URI, segment: String): Plantain#URI = {
-    val underlying = uri.underlying
-    val path = underlying.path / segment
-    URI(underlying.copy(path = path))
-  }
-
-  def relativize(uri: Plantain#URI, other: Plantain#URI): Plantain#URI = {
-    // TODO should rely on a spray.http.Uri when https://github.com/spray/spray/issues/818 is addressed
-    // for implementation algorithm, see https://github.com/stain/cxf/blob/trunk/rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/utils/HttpUtils.java
-    import java.net.{ URI => jURI }
-    val juri = new jURI(uri.underlying.toString).relativize(new jURI(other.underlying.toString))
-    PlantainOps.makeUri(juri.toString)
-  }
-
-  def newChildUri(uri: Plantain#URI): Plantain#URI = {
-    val segment = java.util.UUID.randomUUID().toString.replace("-", "")
-    appendSegment(uri, segment)
-  }
-
-  def lastSegment(uri: Plantain#URI): String =
-    uri.underlying.path.reverse.head.toString
-
-}
-
 object PlantainOps extends RDFOps[Plantain] with PlantainURIOps {
 
   // graph
