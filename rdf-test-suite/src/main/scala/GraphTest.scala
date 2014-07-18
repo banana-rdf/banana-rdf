19b55276d2c139e19949dc17f91e1d2c3d12a3a5
Henry Story <henry.story@bblfish.net>
2014-07-18 18:56:21 +0200

    forgot tests
    Conflicts:
    	rdf-test-suite/src/main/scala/GraphTest.scala
    	rdf-test-suite/src/main/scala/RDFOpsTest.scala

--- rdf-test-suite/src/main/scala/GraphTest.scala
+++ rdf-test-suite/src/main/scala/GraphTest.scala
@@ -4,49 +4,49 @@ import org.scalatest._
 import org.w3.banana.diesel._
 
 abstract class GraphTest[Rdf <: RDF]()(implicit ops: RDFOps[Rdf])
-    extends WordSpec with Matchers {
+extends WordSpec with Matchers {
 
   import ops._
+  
+  def exuri(foo:String="foo"): Rdf#URI = URI("http://example.com/"+foo)
+  val foo1gr: Rdf#Graph = Graph(Triple(exuri(),rdf("foo"),Literal("foo")))
+  val bar1gr: Rdf#Graph = Graph(Triple(exuri(),rdf("bar"),Literal("bar")))
 
-  def exuri(foo: String = "foo"): Rdf#URI = URI("http://example.com/" + foo)
-  val foo1gr: Rdf#Graph = Graph(Triple(exuri(), rdf("foo"), Literal("foo")))
-  val bar1gr: Rdf#Graph = Graph(Triple(exuri(), rdf("bar"), Literal("bar")))
+  def exbnode(n: Int=1): Rdf#Node = BNode("ex"+n)
 
-  def exbnode(n: Int = 1): Rdf#Node = BNode("ex" + n)
-
-  def bnNameGr(n: Int = 1, name: String) = Graph(Triple(exbnode(n), rdf("knows"), Literal(name)))
+  def bnNameGr(n: Int=1,name: String) = Graph(Triple(exbnode(n),rdf("knows"),Literal(name)))
 
   val foo = (
     exuri()
-    -- rdf("foo") ->- "foo"
-    -- rdf("bar") ->- "bar"
-  ).graph
+      -- rdf("foo") ->- "foo"
+      -- rdf("bar") ->- "bar"
+    ).graph
 
   val fooReference = (
     exuri()
-    -- rdf("foo") ->- "foo"
-    -- rdf("bar") ->- "bar"
-  ).graph
+      -- rdf("foo") ->- "foo"
+      -- rdf("bar") ->- "bar"
+    ).graph
 
   val bar = (
     exuri()
-    -- rdf("bar") ->- "bar"
-    -- rdf("baz") ->- "baz"
-  ).graph
+      -- rdf("bar") ->- "bar"
+      -- rdf("baz") ->- "baz"
+    ).graph
 
   val barReference = (
     exuri()
-    -- rdf("bar") ->- "bar"
-    -- rdf("baz") ->- "baz"
-  ).graph
+      -- rdf("bar") ->- "bar"
+      -- rdf("baz") ->- "baz"
+    ).graph
 
   val foobar = (
     exuri()
-    -- rdf("foo") ->- "foo"
-    -- rdf("bar") ->- "bar"
-    -- rdf("baz") ->- "baz"
-  ).graph
-
+      -- rdf("foo") ->- "foo"
+      -- rdf("bar") ->- "bar"
+      -- rdf("baz") ->- "baz"
+    ).graph
+  
   "union tests" when {
 
     "union must compute the union of two graphs, and should not touch the graphs" in {
@@ -67,44 +67,47 @@ abstract class GraphTest[Rdf <: RDF]()(implicit ops: RDFOps[Rdf])
       isomorphism(result, foo) should be(true)
     }
   }
-
+  
   "diff tests" when {
     "removing one triple in a 1 triple graph must return the empty graph" in {
-      isomorphism(diff(foo1gr, foo1gr), emptyGraph) should be(true)
+      isomorphism(diff(foo1gr,foo1gr),emptyGraph) should be(true)
     }
+
 
     "removing one triple in a 2 triple graph must leave the other triple in the graph" in {
 
       val d = diff(foo1gr union bar1gr, foo1gr)
-      isomorphism(d, bar1gr) should be(true)
+      isomorphism(d,bar1gr) should be(true)
 
       val oneBNGraph = bnNameGr(1, "Henry") union bnNameGr(1, "Alexandre")
       val d2 = diff(oneBNGraph, bnNameGr(1, "Alexandre"))
-      isomorphism(d2, bnNameGr(1, "Henry")) should be(true)
+      isomorphism(d2,bnNameGr(1, "Henry")) should be(true)
+
 
       val twoBNGraph = bnNameGr(1, "Henry") union bnNameGr(2, "Alexandre")
       val d3 = diff(twoBNGraph, bnNameGr(2, "Alexandre"))
-      isomorphism(d3, bnNameGr(1, "Henry")) should be(true)
+      isomorphism(d3,bnNameGr(1, "Henry")) should be(true)
 
     }
 
     "removing a triple that is not present in a 2 triple graph must return the same graph" in {
-      val d = diff(foo1gr union bar1gr, bnNameGr(1, "George"))
-      isomorphism(d, foo1gr union bar1gr) should be(true)
+      val d = diff(foo1gr union bar1gr, bnNameGr(1,"George"))
+      isomorphism(d,foo1gr union bar1gr) should be(true)
 
       val oneBNGraph = bnNameGr(1, "Henry") union bnNameGr(1, "Alexandre")
       val d2 = diff(oneBNGraph, bnNameGr(1, "{}"))
-      isomorphism(d2, oneBNGraph) should be(true)
+      isomorphism(d2,oneBNGraph) should be(true)
+
 
       val twoBNGraph = bnNameGr(1, "Henry") union bnNameGr(2, "Alexandre")
       val d3 = diff(twoBNGraph, foo1gr)
-      isomorphism(d3, twoBNGraph) should be(true)
+      isomorphism(d3,twoBNGraph) should be(true)
 
-      val d4 = diff(twoBNGraph, bnNameGr(1, "And now for something completely different"))
-      isomorphism(d4, twoBNGraph) should be(true)
+      val d4 = diff(twoBNGraph, bnNameGr(1,"And now for something completely different"))
+      isomorphism(d4,twoBNGraph) should be(true)
 
     }
-
+    
   }
 
 }
--- rdf-test-suite/src/main/scala/RDFOpsTest.scala
+++ rdf-test-suite/src/main/scala/RDFOpsTest.scala
@@ -5,52 +5,53 @@ import org.w3.banana.diesel._
 import org.scalatest._
 
 abstract class RDFOpsTest[Rdf <: RDF]()(implicit ops: RDFOps[Rdf])
-    extends WordSpec with Matchers with OptionValues {
+extends WordSpec with Matchers with OptionValues {
 
   import ops._
 
   "(de)constructing plain literal" in {
-
+    
     val plainLiteral: Rdf#Literal = Literal("Alexandre")
 
     val (lexicalForm, datatype, langOpt) = fromLiteral(plainLiteral)
 
-    lexicalForm should be("Alexandre")
+    lexicalForm should be ("Alexandre")
 
-    datatype should be(xsd.string)
+    datatype should be (xsd.string)
 
-    langOpt should be(None)
+    langOpt should be (None)
 
   }
 
   "(de)constructing typed literal" in {
-
+    
     val foaf = FOAFPrefix[Rdf]
 
     val typedLiteral: Rdf#Literal = Literal("Alexandre", foaf.name)
 
     val (lexicalForm, datatype, langOpt) = fromLiteral(typedLiteral)
 
-    lexicalForm should be("Alexandre")
+    lexicalForm should be ("Alexandre")
 
-    datatype should be(foaf.name)
+    datatype should be (foaf.name)
 
-    langOpt should be(None)
+    langOpt should be (None)
 
   }
 
   "(de)constructing lang literal" in {
-
+    
     val langLiteral: Rdf#Literal = Literal.tagged("Alexandre", Lang("fr"))
 
     val (lexicalForm, datatype, langOpt) = fromLiteral(langLiteral)
 
-    lexicalForm should be("Alexandre")
+    lexicalForm should be ("Alexandre")
 
-    datatype should be(Literal.rdfLangString)
+    datatype should be (Literal.rdfLangString)
 
-    langOpt.value should be("fr")
+    langOpt.value should be ("fr")
 
   }
+
 
 }
