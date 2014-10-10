8cda967bbf349308457e48afe636427bfe9fca30
Henry Story <henry.story@bblfish.net>
2014-10-10 21:27:01 +0200

    remove two methods that are never used

--- rdf/common/src/main/scala/org/w3/banana/RDFReader.scala
+++ rdf/common/src/main/scala/org/w3/banana/RDFReader.scala
@@ -5,11 +5,6 @@ import java.io._
 import scala.concurrent.Future
 import scala.util._
 
-object RDFReader {
-
-  def apply[Rdf <: RDF, S](implicit rdfReader: RDFReader[Rdf, S]): RDFReader[Rdf, S] = rdfReader
-
-}
 
 trait RDFReader[Rdf <: RDF, +S] {
 
