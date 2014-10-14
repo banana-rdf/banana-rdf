bf33be163a8a90392353554946bd6822a4d8b1c4
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2014-10-14 15:42:40 -0700

    Simplified the test per @betehess' request

--- rdf-test-suite/jvm/src/main/scala/org/w3/banana/diesel/DieselOntologyGraphConstructTest.scala
+++ rdf-test-suite/jvm/src/main/scala/org/w3/banana/diesel/DieselOntologyGraphConstructTest.scala
@@ -51,108 +51,55 @@ abstract class DieselOntologyGraphConstructTest[Rdf <: RDF]()(
     )
     val g1 = union( o1 map (_.graph) )
     
-    val o2 = Seq(
-      bURI("o2").a(owl.Ontology),
-      bURI("c1").a(owl.Class),
-      bURI("c2").a(owl.Class),
-      bURI("c3").a(owl.Class),
-      bURI("c1") -- rdfs.subClassOf ->- (
-          bnode().a(owl.Class)
-          -- owl.unionOf ->- List( bURI("c2"), bURI("c3"))
-          ),
-      bURI("c3") -- rdfs.label ->- "c3"
-    )
-    val g2 = union( o2 map (_.graph) )
-    
     val t1 =  Triple(URI("http://example.org/o1"), rdf.`type`, owl.Ontology)
     val t2 =  Triple(URI("http://example.org/o2"), rdf.`type`, owl.Ontology)
         	   
     "tbox should have the correct ontology IRI" in {
       g1 contains(t1) should be(true)
-      g2 contains(t2) should be(true)
+      g1 contains(t2) should be(false)
     }
     
     val tboxTurtleRep1 = turtleWriter.asString(g1, ex1)
     assert(tboxTurtleRep1.isSuccess)
-    println("------------------------")
-    println(tboxTurtleRep1.get)
-    println("------------------------")
     val tboxTurtleGraph1a = Await.result(turtleReader.read(tboxTurtleRep1.get, ex1 ), Duration(1, MINUTES))
     val tboxTurtleGraph1b = Await.result(turtleReader.read(tboxTurtleRep1.get, ex2 ), Duration(1, MINUTES))
     
-    val tboxTurtleRep2 = turtleWriter.asString(g2, ex1)
-    assert(tboxTurtleRep2.isSuccess)
-    println("------------------------")
-    println(tboxTurtleRep2.get)
-    println("------------------------")
-    val tboxTurtleGraph2a = Await.result(turtleReader.read(tboxTurtleRep2.get, ex2 ), Duration(1, MINUTES))
-    val tboxTurtleGraph2b = Await.result(turtleReader.read(tboxTurtleRep2.get, ex1 ), Duration(1, MINUTES))
-    
     "serializing to turtle & reading the graph back with the same base shouldn't change the ontology IRI" in {
       tboxTurtleGraph1a contains(t1) should be(true)
-      tboxTurtleGraph1a contains(t2) should be(false)
-      
-      tboxTurtleGraph2a contains(t1) should be(false)
-      tboxTurtleGraph2a contains(t2) should be(true)
     }
     
     "serializing to turtle & reading the graph back with a different base shouldn't change the ontology IRI" in {
       tboxTurtleGraph1b contains(t1) should be(true)
-      tboxTurtleGraph1b contains(t2) should be(false)
-      
-      tboxTurtleGraph2b contains(t1) should be(false)
-      tboxTurtleGraph2b contains(t2) should be(true)
     }
     
     val tboxRdfXMLRep1 = rdfXMLWriter.asString(g1, ex1)
-    assert(tboxRdfXMLRep1.isSuccess)
-    println("------------------------")
-    println(tboxRdfXMLRep1.get)
-    println("------------------------")
+    require(tboxRdfXMLRep1.isSuccess, tboxRdfXMLRep1.failed.get)
     val tboxRdfXMLGraph1a = Await.result(rdfXMLReader.read(tboxRdfXMLRep1.get, ex1), Duration(1, MINUTES))
     val tboxRdfXMLGraph1b = Await.result(rdfXMLReader.read(tboxRdfXMLRep1.get, ex2), Duration(1, MINUTES))
-    
-    val tboxRdfXMLRep2 = rdfXMLWriter.asString(g2, ex2)
-    assert(tboxRdfXMLRep2.isSuccess)
-    println("------------------------")
-    println(tboxRdfXMLRep2.get)
-    println("------------------------")
-    val tboxRdfXMLGraph2a = Await.result(rdfXMLReader.read(tboxRdfXMLRep2.get, ex2), Duration(1, MINUTES))
-    val tboxRdfXMLGraph2b = Await.result(rdfXMLReader.read(tboxRdfXMLRep2.get, ex1), Duration(1, MINUTES))
     
    "serializing to RDF/XML & reading the graph back with the same base shouldn't change the ontology IRI" in {
       tboxRdfXMLGraph1a contains(t1) should be(true)
       tboxRdfXMLGraph1a contains(t2) should be(false)
-      
-      tboxRdfXMLGraph2a contains(t1) should be(false)
-      tboxRdfXMLGraph2a contains(t2) should be(true)
     }
     "serializing to RDF/XML & reading the graph back with a different base shouldn't change the ontology IRI" in {
       tboxRdfXMLGraph1b contains(t1) should be(true)
       tboxRdfXMLGraph1b contains(t2) should be(false)
-      
-      tboxRdfXMLGraph2b contains(t1) should be(false)
-      tboxRdfXMLGraph2b contains(t2) should be(true)
     }
     
     "reading the serializations to Turtle & to RDF/XML with the same base should produce isomorphic graphs" in {
       tboxTurtleGraph1a isIsomorphicWith tboxRdfXMLGraph1a should be(true)
-      tboxTurtleGraph2a isIsomorphicWith tboxRdfXMLGraph2a should be(true)
     }
     
     "reading the serializations to Turtle & to RDF/XML with the different Turtle base should produce isomorphic graphs" in {
       tboxTurtleGraph1b isIsomorphicWith tboxRdfXMLGraph1a should be(true)
-      tboxTurtleGraph2b isIsomorphicWith tboxRdfXMLGraph2a should be(true)
     }
     
     "reading the serializations to Turtle & to RDF/XML with the different RDF/XML base should produce isomorphic graphs" in {
       tboxTurtleGraph1a isIsomorphicWith tboxRdfXMLGraph1b should be(true)
-      tboxTurtleGraph2a isIsomorphicWith tboxRdfXMLGraph2b should be(true)
     }
     
     "reading the serializations to Turtle & to RDF/XML with different bases should produce isomorphic graphs" in {
       tboxTurtleGraph1b isIsomorphicWith tboxRdfXMLGraph1b should be(true)
-      tboxTurtleGraph2b isIsomorphicWith tboxRdfXMLGraph2b should be(true)
     }
   }
 
