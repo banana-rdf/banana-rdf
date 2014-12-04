06cd9948954df9a958499e441f902e1c3fc5b536
antonkulaga <antonkulaga@gmail.com>
2014-12-04 03:02:30 +0200

    fix for wrong diesel resolution

--- rdf/common/src/main/scala/org/w3/banana/diesel/diesel.scala
+++ rdf/common/src/main/scala/org/w3/banana/diesel/diesel.scala
@@ -6,6 +6,10 @@ trait Diesel {
 
   implicit def toPointedGraphW[Rdf <: RDF](node: Rdf#Node)(implicit ops: RDFOps[Rdf]): PointedGraphW[Rdf] = new PointedGraphW[Rdf](PointedGraph(node)(ops))
 
+  implicit def toPointedGraphURIW[Rdf <: RDF](node: Rdf#URI)(implicit ops: RDFOps[Rdf]): PointedGraphW[Rdf] = new PointedGraphW[Rdf](PointedGraph(node)(ops))
+
+  implicit def toPointedGraphBNodeW[Rdf <: RDF](node: Rdf#BNode)(implicit ops: RDFOps[Rdf]): PointedGraphW[Rdf] = new PointedGraphW[Rdf](PointedGraph(node)(ops))
+
   implicit def toPointedGraphW[Rdf <: RDF](pointed: PointedGraph[Rdf]): PointedGraphW[Rdf] = new PointedGraphW[Rdf](pointed)
 
 }
