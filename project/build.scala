0bb775053a5d30a4921545ef18698c4f45ed2ac1
Alexandre Bertails <alexandre@bertails.org>
2015-01-13 18:27:09 -0500

    [build] Bump to 0.7.3.radical-SNAPSHOT.

--- project/build.scala
+++ project/build.scala
@@ -17,7 +17,7 @@ object BuildSettings {
 
   val buildSettings = publicationSettings ++ defaultScalariformSettings ++ Seq(
     organization := "org.w3",
-    version := "0.7.2.radical",
+    version := "0.7.3.radical-SNAPSHOT",
     scalaVersion := "2.11.4",
     crossScalaVersions := Seq("2.11.4", "2.10.4"),
     javacOptions ++= Seq("-source", "1.7", "-target", "1.7"),
