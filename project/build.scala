809b936182bbdb86ff2d8d39fd69ba0e981954c3
Henry Story <henry.story@bblfish.net>
2015-05-07 14:24:43 +0200

    update to latest scala & scala-js

--- project/build.scala
+++ project/build.scala
@@ -13,8 +13,8 @@ object BuildSettings {
 
   val buildSettings = publicationSettings ++ defaultScalariformSettings ++ Seq(
     organization := "org.w3",
-    scalaVersion := "2.11.5",
-    crossScalaVersions := Seq("2.11.5", "2.10.4"),
+    scalaVersion := "2.11.6",
+    crossScalaVersions := Seq("2.11.6", "2.10.4"),
     javacOptions ++= Seq("-source", "1.7", "-target", "1.7"),
     fork := false,
     parallelExecution in Test := false,
