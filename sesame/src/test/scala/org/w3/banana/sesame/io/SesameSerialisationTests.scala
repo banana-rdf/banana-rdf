b1c7a89a81521840263a6a01ada5ea8f067cc9d3
Trond Marius Øvstetun <trond.marius.ovstetun@nrk.no>
2017-10-26 12:44:29 +0200

    Print prefixes when using Sesame as well as Jena.

--- sesame/src/test/scala/org/w3/banana/sesame/io/SesameSerialisationTests.scala
+++ sesame/src/test/scala/org/w3/banana/sesame/io/SesameSerialisationTests.scala
@@ -1,11 +1,42 @@
 package org.w3.banana.sesame.io
 
+import org.w3.banana.Prefix
 import org.w3.banana.io._
 import org.w3.banana.sesame._
+
 import scala.util.Try
 import org.w3.banana.util.tryInstances._
+import scalaz.syntax._, comonad._
 
-class SesameTurtleTests extends TurtleTestSuite[Sesame, Try]
+class SesameTurtleTests extends TurtleTestSuite[Sesame, Try] {
+
+  "write with prefixes" in {
+    val prefix = Prefix[Sesame]("foo", "http://purl.org/dc/elements/1.1/")
+
+    //    val expectedString =
+    //      """
+    //        |@prefix foo:   <http://purl.org/dc/elements/1.1/> .
+    //        |
+    //        |<http://www.w3.org/2001/sw/RDFCore/ntriples/>
+    //        |        foo:creator    "Dave Beckett" , "Art Barstow" ;
+    //        |        foo:publisher  <http://www.w3.org/> .""".stripMargin
+
+    val withPrefix = writer.asString(referenceGraph, "", prefix).copoint
+    withPrefix should include("@prefix foo:")
+    withPrefix should include("foo:creator")
+    withPrefix should include("foo:publisher")
+    withPrefix should not include ("<http://purl.org/dc/elements/1.1/creator>")
+    withPrefix should not include ("<http://purl.org/dc/elements/1.1/publisher>")
+
+    val noPrefix = writer.asString(referenceGraph, "").copoint
+    noPrefix should not include ("@prefix foo:")
+    noPrefix should not include ("foo:creator")
+    noPrefix should not include ("foo:publisher")
+    noPrefix should include("<http://purl.org/dc/elements/1.1/creator>")
+    noPrefix should include("<http://purl.org/dc/elements/1.1/publisher>")
+
+  }
+}
 
 class SesameNTripleReaderTestSuite extends NTriplesReaderTestSuite[Sesame]
 
