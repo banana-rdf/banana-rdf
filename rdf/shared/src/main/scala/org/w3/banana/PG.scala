2f13ab2019d0db9bc70120d8ae4a48f0c6aff892
Henry Story <henry.story@bblfish.net>
2021-09-10 19:15:16 +0200

    does not compile with PG[Rdf<:RDF](point: Node[Rdf],...)

--- rdf/shared/src/main/scala/org/w3/banana/PG.scala
+++ rdf/shared/src/main/scala/org/w3/banana/PG.scala
@@ -1,31 +1,17 @@
-//package org.w3.banana
+package org.w3.banana
 
-//import org.w3.banana.RDF
-//
-//
-//trait RDFOps2[T <: RDFObj](using val rdf: T) {
-//  def emptyGraph: rdf.Graph
-//  def fromUri(uri: rdf.URI): String
-//  def makeUri(s: String): rdf.URI
-//}
-//
-//trait PointedGraph2[Rdf <: RDFObj] {
-//  def pointer: Rdf#Node
-//  def graph: Rdf#Graph
-//}
-//import RDF.*
-//
-//trait PG[Rdf <: RDF](val pointer: Node[Rdf], val graph: Graph[Rdf])
-//
-//object PG:
-//	def apply[Rdf <: RDF](_pointer: Node[Rdf], _graph: Graph[Rdf]): PG[Rdf] =
-//		new PG[Rdf] {
-//			val pointer = _pointer
-//			val graph = _graph
-//		}
-//
-//	def apply[Rdf <: RDF](node: Node[Rdf])(using rdf: Rdf): PG[Rdf] =
-//		PG(node, rdf.Graph.empty)
-//
-//	def unapply[Rdf <: RDF](pg: PG[Rdf]): Option[(Node[Rdf], Graph[Rdf])] =
-//		Some((pg.pointer, pg.graph))
+import org.w3.banana.RDF
+
+import RDF.*
+
+trait PG[Rdf <: RDF](val pointer: Node[Rdf], val graph: Graph[Rdf])
+
+object PG:
+	def apply[Rdf <: RDF](pointer: Node[Rdf], graph: Graph[Rdf]): PG[Rdf] =
+		new PG[Rdf](pointer, graph)
+
+	def apply[Rdf <: RDF](node: Node[Rdf])(using rdf: Rdf): PG[Rdf] =
+		PG[Rdf](node, rdf.Graph.empty)
+
+	def unapply[Rdf <: RDF](pg: PG[Rdf]): Option[(Node[Rdf], Graph[Rdf])] =
+		Some((pg.pointer, pg.graph))
