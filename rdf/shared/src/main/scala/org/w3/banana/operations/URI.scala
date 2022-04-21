1968f38e4d80b3fffb69a9c013aecca4844a087c
Gunnar André Saxvik-Lilleaasen <gunnar.andre.saxvik-lilleaasen@sikt.no>
2022-04-21 12:09:14 +0000

    Added relativize to URI.

--- rdf/shared/src/main/scala/org/w3/banana/operations/URI.scala
+++ rdf/shared/src/main/scala/org/w3/banana/operations/URI.scala
@@ -13,14 +13,19 @@
 
 package org.w3.banana.operations
 
+import java.net.URI as jURI
+
 import scala.util.Try
 import org.w3.banana.RDF
+import org.w3.banana.Ops
 
 object URI:
    val xsdStr: String     = "http://www.w3.org/2001/XMLSchema#string"
    val xsdLangStr: String = "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString"
 
-trait URI[Rdf <: RDF]:
+trait URI[Rdf <: RDF](using ops: Ops[Rdf]):
+   import ops.*
+
    /** (can) throw an exception (depending on implementation of URI) different implementations
      * decide to parse at different points, and do varying quality jobs at that (check). Need to
      * look at how capability based exceptions could help
@@ -29,7 +34,11 @@ trait URI[Rdf <: RDF]:
    def apply(uriStr: String): RDF.URI[Rdf] = mkUri(uriStr).get
    def mkUri(iriStr: String): Try[RDF.URI[Rdf]]
    protected def asString(uri: RDF.URI[Rdf]): String
+   protected def asRelativeURI(uri: RDF.URI[Rdf], other: RDF.URI[Rdf]): Option[RDF.rURI[Rdf]] =
+      val rel = jURI(uri.value).relativize(jURI(other.value)).nn
+      Option.unless(rel.isAbsolute())(rURI(rel.toString))
    extension (uri: RDF.URI[Rdf])
-      def value: String                     = asString(uri)
-      def ===(other: RDF.URI[Rdf]): Boolean = uri.equals(other)
+      def value: String                                          = asString(uri)
+      def ===(other: RDF.URI[Rdf]): Boolean                      = uri.equals(other)
+      def relativize(other: RDF.URI[Rdf]): Option[RDF.rURI[Rdf]] = asRelativeURI(uri, other)
 end URI
